79ca3075eaf14a99d4a8df5116f17c53f1a3052e
md Hamimul Haue <mdhamimulhaque@gmail.com>
2023-12-21 20:58:02 +0600

    error handler setup

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 const toolsRoutes = require("./routes/v1/tools.route");
+const errorHandler = require("./middlewares/errorHandler");
 app.use(cors());
 app.use(express.json());
 app.set("view engin", "ejs");
@@ -31,6 +32,16 @@ app.all("*", (req, res) => {
   res.send("No Route Not Found");
 });
 
+app.use(errorHandler);
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+
+// ---> global error handler
+process.on("unhandledRejection", (error) => {
+  console.log(error.name, error.message);
+  app.close(() => {
+    process.exit(1);
+  });
 });
