e6baa3e452b91e77322a1de951314ca404d6f6f6
md Hamimul Haue <mdhamimulhaque@gmail.com>
2023-12-21 12:08:55 +0600

    basic server setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+async function run() {
+  try {
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
