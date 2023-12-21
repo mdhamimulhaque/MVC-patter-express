7af182a85af9c1bc5651d9bfb4ad0df2fb777fd3
md Hamimul Haue <mdhamimulhaque@gmail.com>
2023-12-21 12:35:00 +0600

    modify routes system to avid DRY

--- routes/v1/tools.route.js
+++ routes/v1/tools.route.js
@@ -2,11 +2,20 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("get all tools");
-});
-router.post("/", (req, res) => {
-  res.send("post a tool");
-});
+// router.get("/", (req, res) => {
+//   res.send("get all tools");
+// });
+// router.post("/", (req, res) => {
+//   res.send("post a tool");
+// });
+
+router
+  .route("/")
+  .get((req, res) => {
+    res.send("get all tools here ");
+  })
+  .post((req, res) => {
+    res.send("add a tool");
+  });
 
 module.exports = router;
