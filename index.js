31cb7b904acf90c4911a86e0212c383d1d53e98b
md Hamimul Haue <mdhamimulhaque@gmail.com>
2023-12-21 20:34:27 +0600

    using (ejs) for showing dynamic content with html file from a server

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 const toolsRoutes = require("./routes/v1/tools.route");
 app.use(cors());
 app.use(express.json());
+app.set("view engin", "ejs");
 
 app.use("/api/v1/tools", toolsRoutes);
 
@@ -17,7 +18,13 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render("index.ejs", {
+    id: 123,
+    user: {
+      name: "hamim",
+      age: 24,
+    },
+  });
 });
 
 app.all("*", (req, res) => {
